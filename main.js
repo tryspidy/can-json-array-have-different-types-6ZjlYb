bc4b19ed59a9fa289852107faa67f6d7290e6626
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-07 19:52:12 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+In JSON, each element in an array may be of a different type. 
+In Python, "array" is analogous to a list or tuple type, 
+  depending on usage. However, the json module in the Python 
+  standard library will always use Python lists to represent JSON arrays.
